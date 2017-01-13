c75dd1d51dd3357d975be06da8e7b03277f1c23a
mario-digitalstate <mario@digitalstate.ca>
2017-01-13 15:40:54 -0500

    Add abstract api rest controller.

---
+++ Controller/Api/Rest/AbstractController.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace Ds\Bundle\ApiBundle\Controller\Api\Rest;
+
+use Oro\Bundle\SoapBundle\Controller\Api\Rest\RestController;
+use Doctrine\Common\Collections\ArrayCollection;
+use Oro\Bundle\LocaleBundle\Entity\FallbackTrait;
+
+/**
+ * Class AbstractController
+ */
+abstract class AbstractController extends RestController
+{
+    use FallbackTrait;
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function transformEntityField($field, &$value)
+    {
+        switch ($field) {
+            case 'owner':
+            case 'organization':
+                $value = $this->transformEntityToId($value);
+                break;
+
+            default:
+                parent::transformEntityField($field, $value);
+        }
+    }
+
+    /**
+     * Transform localized values to texts.
+     *
+     * @param \Doctrine\Common\Collections\ArrayCollection $values
+     * @return array
+     */
+    protected function transformLocalizedValuesToTexts($values)
+    {
+        $array = [];
+
+        foreach ($values as $value) {
+            $localization = $value->getLocalization();
+            $code = $localization ? $localization->getFormattingCode() : 'default';
+            $text = $this->getLocalizedFallbackValue($values, $localization)->getText();
+            $array[$code] = $text;
+        }
+
+        return $array;
+    }
+
+    /**
+     * Transform entity to id.
+     *
+     * @param object $value
+     * @return mixed
+     */
+    protected function transformEntityToId($value)
+    {
+        if ($value) {
+            $value = $value->getId();
+        }
+
+        return $value;
+    }
+
+    /**
+     * Transform entities to ids.
+     *
+     * @param \Doctrine\Common\Collections\ArrayCollection $values
+     * @return array
+     */
+    protected function transformEntitiesToIds($values)
+    {
+        $array = [];
+
+        foreach ($values as $value) {
+            $array[] = $this->transformEntityToId($value);
+        }
+
+        return $array;
+    }
+}
