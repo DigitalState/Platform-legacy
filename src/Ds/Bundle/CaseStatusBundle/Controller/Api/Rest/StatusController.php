74aa98db66f5829c166ff9e62cc983656921a74e
mario-digitalstate <mario@digitalstate.ca>
2017-01-13 15:34:16 -0500

    Add transformEntityField method to api controller.

--- src/Ds/Bundle/CaseStatusBundle/Controller/Api/Rest/StatusController.php
+++ src/Ds/Bundle/CaseStatusBundle/Controller/Api/Rest/StatusController.php
@@ -2,7 +2,7 @@
 
 namespace Ds\Bundle\CaseStatusBundle\Controller\Api\Rest;
 
-use Oro\Bundle\SoapBundle\Controller\Api\Rest\RestController;
+use Ds\Bundle\ApiBundle\Controller\Api\Rest\AbstractController;
 
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use FOS\RestBundle\Controller\Annotations\NamePrefix;
@@ -17,7 +17,7 @@ use Oro\Bundle\SecurityBundle\Annotation\AclAncestor;
  * @Route("/case")
  * @NamePrefix("ds_casestatus_api_rest_")
  */
-class StatusController extends RestController
+class StatusController extends AbstractController
 {
     /**
      * Get collection action
@@ -106,5 +106,25 @@ class StatusController extends RestController
     public function getManager()
     {
         return $this->get('ds.casestatus.manager.status');
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function transformEntityField($field, &$value)
+    {
+        switch ($field) {
+            case 'case':
+                $value = $this->transformEntityToId($value);
+                break;
+
+            case 'titles':
+            case 'descriptions':
+                $value = $this->transformLocalizedValuesToTexts($value);
+                break;
+
+            default:
+                parent::transformEntityField($field, $value);
+        }
     }
 }
