a74a6a01d46842c772d6758f018fd13881c34c36
mario-digitalstate <mario@digitalstate.ca>
2017-01-16 12:15:45 -0500

    Configure entity fallback localization fields in api controller.

--- src/Ds/Bundle/ServiceBundle/Controller/Api/Rest/ServiceController.php
+++ src/Ds/Bundle/ServiceBundle/Controller/Api/Rest/ServiceController.php
@@ -123,4 +123,12 @@ class ServiceController extends AbstractController
                 parent::transformEntityField($field, $value);
         }
     }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function getFallbackLocalizationFields()
+    {
+        return [ 'titles', 'descriptions', 'buttons', 'presentations' ];
+    }
 }
