485232b9a5b878d77e1ffa976cef06b1c5cb20a5
mario-digitalstate <mario@digitalstate.ca>
2017-01-17 16:34:11 -0500

    Setup portal and account navigation menus and removed hard-coded menu links to appropriate bundles.

---
+++ src/Ds/Bundle/AccountUserBundle/DsAccountUserBundle.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Ds\Bundle\AccountUserBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+/**
+ * Class DsAccountUserBundle
+ */
+class DsAccountUserBundle extends Bundle
+{
+}
