d041605dc431881e83701154a6b69406092463be
mario-digitalstate <mario@digitalstate.ca>
2017-01-23 14:28:13 -0500

    Removed hard-coded dashboard code.

--- src/Ds/Bundle/AccountDashboardBundle/Controller/Account/DashboardController.php
+++ src/Ds/Bundle/AccountDashboardBundle/Controller/Account/DashboardController.php
@@ -23,35 +23,35 @@ class DashboardController extends Controller
      */
     public function indexAction()
     {
-        $user = $this->getUser();
-
-        $manager = $this->get('ds.case.manager.case');
-        $cases = $manager->getList(3, 1, [ 'user' => $user ], [ 'createdAt' => 'DESC' ]);
-        $caseCount = count($manager->getList(null, null, [ 'user' => $user ]));
-
-        $manager = $this->get('ds.casestatus.manager.status');
-        $statuses = [];
-
-        foreach ($cases as $case) {
-            $status = $manager->getList(1, 1, [ 'case' => $case ], [ 'createdAt' => 'DESC' ]);
-            $statuses[$case->getId()] = array_shift($status);
-        }
-
-        $bpm = $this->get('ds.bpm.bpm.api.factory')->create('camunda');
-        $taskCount = $bpm->getTaskCount([ 'assignee' => $user->getId() ]);
-
-        $manager = $this->get('ds.service.manager.service');
-        $services = $manager->getList(3, 1);
-
+//        $user = $this->getUser();
+//
+//        $manager = $this->get('ds.case.manager.case');
+//        $cases = $manager->getList(3, 1, [ 'user' => $user ], [ 'createdAt' => 'DESC' ]);
+//        $caseCount = count($manager->getList(null, null, [ 'user' => $user ]));
+//
+//        $manager = $this->get('ds.casestatus.manager.status');
+//        $statuses = [];
+//
+//        foreach ($cases as $case) {
+//            $status = $manager->getList(1, 1, [ 'case' => $case ], [ 'createdAt' => 'DESC' ]);
+//            $statuses[$case->getId()] = array_shift($status);
+//        }
+//
+//        $bpm = $this->get('ds.bpm.bpm.api.factory')->create('camunda');
+//        $taskCount = $bpm->getTaskCount([ 'assignee' => $user->getId() ]);
+//
+//        $manager = $this->get('ds.service.manager.service');
+//        $services = $manager->getList(3, 1);
+//
 //        $manager = $this->get('ds.notification.manager.message');
 //        $messages = $manager->getList(3, 1, [ 'user' => $user ], [ 'sentAt' => 'DESC' ]);
 
         return [
-            'cases' => $cases,
-            'caseCount' => $caseCount,
-            'statuses' => $statuses,
-            'taskCount' => $taskCount,
-            'services' => $services,
+//            'cases' => $cases,
+//            'caseCount' => $caseCount,
+//            'statuses' => $statuses,
+//            'taskCount' => $taskCount,
+//            'services' => $services,
 //            'messages' => $messages
         ];
     }
