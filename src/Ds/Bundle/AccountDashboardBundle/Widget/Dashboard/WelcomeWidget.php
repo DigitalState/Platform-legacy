3ba5955b59480b3cf0e1a616749e658bd5ac11e7
mario-digitalstate <mario@digitalstate.ca>
2017-01-23 15:19:30 -0500

    Add welcome widget to dashboard index.

---
+++ src/Ds/Bundle/AccountDashboardBundle/Widget/Dashboard/WelcomeWidget.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Ds\Bundle\AccountDashboardBundle\Widget\Dashboard;
+
+use Ds\Bundle\WidgetBundle\Widget\Widget;
+
+/**
+ * Class WelcomeWidget
+ */
+class WelcomeWidget extends Widget
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function getTitle()
+    {
+        return '';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getContent(array $data = [])
+    {
+        return $this->templating->render('@DsAccountDashboardBundle/Resources/views/Account/Dashboard/widget/welcome.html.twig', $data);
+    }
+}
