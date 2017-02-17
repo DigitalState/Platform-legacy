e041052ed03e58b690025fe12daadb931c004244
mario-digitalstate <mario@digitalstate.ca>
2017-02-17 13:05:35 -0500

    Squashed 'src/Ds/Bundle/BpmCamundaBundle/' content from commit 161b2ec
    git-subtree-dir: src/Ds/Bundle/BpmCamundaBundle
    git-subtree-split: 161b2ec8028ebf6702799172a63036d6ab100132

---
+++ DependencyInjection/DsBpmCamundaExtension.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Ds\Bundle\BpmCamundaBundle\DependencyInjection;
+
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\Config\FileLocator;
+use Symfony\Component\HttpKernel\DependencyInjection\Extension;
+use Symfony\Component\DependencyInjection\Loader;
+
+/**
+ * Class DsBpmCamundaExtension
+ */
+class DsBpmCamundaExtension extends Extension
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function load(array $configs, ContainerBuilder $container)
+    {
+        $configuration = new Configuration;
+        $config = $this->processConfiguration($configuration, $configs);
+
+        $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
+        $loader->load('bpm.yml');
+        $loader->load('commands.yml');
+
+        $container->prependExtensionConfig($this->getAlias(), array_intersect_key($config, array_flip([ 'settings' ])));
+    }
+}
---
+++ README.md
@@ -0,0 +1,13 @@
+# Platform-Bpm-Camunda-Bundle
+
+The BpmCamunda bundle extends the [DsBpmBundle](https://github.com/DigitalState/Platform-Bpm-Bundle) and provides the developers Camunda integration.
+
+[![Code Climate](https://codeclimate.com/github/DigitalState/Platform-Bpm-Camunda-Bundle/badges/gpa.svg)](https://codeclimate.com/github/DigitalState/Platform-Bpm-Camunda-Bundle)
+[![Test Coverage](https://codeclimate.com/github/DigitalState/Platform-Bpm-Camunda-Bundle/badges/coverage.svg)](https://codeclimate.com/github/DigitalState/Platform-Bpm-Camunda-Bundle/coverage)
+
+## Table of Contents
+
+- [Todo](#todo)
+
+## Todo
+
