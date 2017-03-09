e795637fe67ad47120a1743621ac48a8f126e819
mario-digitalstate <your email here>
2017-03-09 17:03:10 -0500

    Restructure communication bundle into separate bundles

--- src/Ds/Bundle/MessageBundle/Migrations/Schema/v0_4_0/DsMessageBundle.php
+++ src/Ds/Bundle/MessageBundle/Migrations/Schema/v0_4_0/DsMessageBundle.php
@@ -16,6 +16,62 @@ class DsMessageBundle implements Migration
      */
     public function up(Schema $schema, QueryBag $queries)
     {
+        /** Tables generation **/
+        $this->createDsMessageTable($schema);
 
+        /** Foreign keys generation **/
+        $this->addDsMessageForeignKeys($schema);
+    }
+
+    /**
+     * Create ds_message table
+     *
+     * @param Schema $schema
+     */
+    protected function createDsMessageTable(Schema $schema)
+    {
+        $table = $schema->createTable('ds_message');
+        $table->addColumn('id', 'integer', ['autoincrement' => true]);
+        $table->addColumn('organization_id', 'integer', ['notnull' => false]);
+        $table->addColumn('business_unit_owner_id', 'integer', ['notnull' => false]);
+        $table->addColumn('user_id', 'integer', ['notnull' => false]);
+        $table->addColumn('created_at', 'datetime', []);
+        $table->addColumn('updated_at', 'datetime', []);
+        $table->addColumn('title', 'string', ['length' => 255]);
+        $table->addColumn('presentation', 'text', ['notnull' => false]);
+        $table->addColumn('sent_at', 'datetime', ['notnull' => false]);
+        $table->addColumn('discriminator', 'string', ['length' => 255]);
+        $table->setPrimaryKey(['id']);
+        $table->addIndex(['user_id'], 'IDX_557F3D07A76ED395', []);
+        $table->addIndex(['business_unit_owner_id'], 'IDX_557F3D0759294170', []);
+        $table->addIndex(['organization_id'], 'IDX_557F3D0732C8A3DE', []);
+    }
+
+    /**
+     * Add ds_message foreign keys.
+     *
+     * @param Schema $schema
+     */
+    protected function addDsMessageForeignKeys(Schema $schema)
+    {
+        $table = $schema->getTable('ds_message');
+        $table->addForeignKeyConstraint(
+            $schema->getTable('oro_organization'),
+            ['organization_id'],
+            ['id'],
+            ['onDelete' => 'SET NULL', 'onUpdate' => null]
+        );
+        $table->addForeignKeyConstraint(
+            $schema->getTable('oro_business_unit'),
+            ['business_unit_owner_id'],
+            ['id'],
+            ['onDelete' => 'SET NULL', 'onUpdate' => null]
+        );
+        $table->addForeignKeyConstraint(
+            $schema->getTable('oro_user'),
+            ['user_id'],
+            ['id'],
+            ['onDelete' => null, 'onUpdate' => null]
+        );
     }
 }
--- src/Ds/Bundle/TemplateBundle/Migrations/Schema/v0_4_0/DsTemplateBundle.php
+++ src/Ds/Bundle/TemplateBundle/Migrations/Schema/v0_4_0/DsTemplateBundle.php
@@ -16,6 +16,53 @@ class DsTemplateBundle implements Migration
      */
     public function up(Schema $schema, QueryBag $queries)
     {
+        /** Tables generation **/
+        $this->createDsTemplateTable($schema);
 
+        /** Foreign keys generation **/
+        $this->addDsTemplateForeignKeys($schema);
+    }
+
+    /**
+     * Create ds_template table
+     *
+     * @param Schema $schema
+     */
+    protected function createDsTemplateTable(Schema $schema)
+    {
+        $table = $schema->createTable('ds_template');
+        $table->addColumn('id', 'integer', ['autoincrement' => true]);
+        $table->addColumn('organization_id', 'integer', ['notnull' => false]);
+        $table->addColumn('business_unit_owner_id', 'integer', ['notnull' => false]);
+        $table->addColumn('created_at', 'datetime', []);
+        $table->addColumn('updated_at', 'datetime', []);
+        $table->addColumn('title', 'string', ['length' => 255]);
+        $table->addColumn('presentation', 'text', ['notnull' => false]);
+        $table->addColumn('discriminator', 'string', ['length' => 255]);
+        $table->setPrimaryKey(['id']);
+        $table->addIndex(['business_unit_owner_id'], 'IDX_C95D248059294170', []);
+        $table->addIndex(['organization_id'], 'IDX_C95D248032C8A3DE', []);
+    }
+
+    /**
+     * Add ds_template foreign keys.
+     *
+     * @param Schema $schema
+     */
+    protected function addDsTemplateForeignKeys(Schema $schema)
+    {
+        $table = $schema->getTable('ds_template');
+        $table->addForeignKeyConstraint(
+            $schema->getTable('oro_organization'),
+            ['organization_id'],
+            ['id'],
+            ['onDelete' => 'SET NULL', 'onUpdate' => null]
+        );
+        $table->addForeignKeyConstraint(
+            $schema->getTable('oro_business_unit'),
+            ['business_unit_owner_id'],
+            ['id'],
+            ['onDelete' => 'SET NULL', 'onUpdate' => null]
+        );
     }
 }
